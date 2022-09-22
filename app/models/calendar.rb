31710d2214652fc63d4141b18097bce81be1f9be
Mouaz El Molkey <moaz.mulki@outlook.com>
2022-09-22 19:23:45 +0300

    add validtaion

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,4 +1,4 @@
 class Calendar < ApplicationRecord
+  validates :name, :password, presence: true
 
-    
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :calendar
+  validates :title, :description, :start, :end, :location, :calendar_id, presence: true
+
+
 end
